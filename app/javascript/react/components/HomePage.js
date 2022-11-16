e2d6ded605e3208a3e12747048d1d7867703bc14
Ziming Chen <zimingchen153@gmail.com>
2022-11-16 13:34:30 -0500

    homepage-search-redirect

--- app/javascript/react/components/HomePage.js
+++ app/javascript/react/components/HomePage.js
@@ -2,25 +2,26 @@ import React, {useState} from 'react'
 import { Redirect } from 'react-router-dom'
 
 const HomePageIndex = (props) => {
-  const [searchCity, setSearchCity] = useState({
-    city: ""
-  })
+  const [searchCity, setSearchCity] = useState("")
+  const [redirect, SetRedirect] = useState(false)
 
-  const handleInputChange = (event) => {
-    setSearchCity({
-      ...searchCity,
-      [event.currentTarget.city]: event.currentTarget.value
-    })
+  const handleChange = (event) => {
+    event.preventDefault()
+    setSearchCity(event.target.value)
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    SetRedirect(!redirect)
   }
 
-  if (searchCity.city !== "") {
-    return <Redirect to={`/restaurants`} />
+  if (redirect) {
+    return <Redirect to={`/${searchCity}`}/>
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
-        <input type="text" name="city" onChange={handleInputChange}></input>
+        <input type="text" name="city" onChange={handleChange}/>
       </label>
       <input type="submit" value="Search by City" />
     </form>
