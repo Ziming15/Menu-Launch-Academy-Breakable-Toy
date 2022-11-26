d64cabeb211906cb0d47419c22f44e89edca7fd2
Ziming Chen <zimingchen153@gmail.com>
2022-11-26 16:30:16 -0500

    added description

--- db/migrate/20221118160737_create_foods.rb
+++ db/migrate/20221118160737_create_foods.rb
@@ -2,8 +2,9 @@ class CreateFoods < ActiveRecord::Migration[5.2]
   def change
     create_table :foods do |t|
       t.string :name, null: false
-      t.string :image_url, null: false
+      t.string :image_url 
       t.string :flavor, null: false
+      t.text :description, null: false
       t.string :restaurant_id, null: false
 
       
