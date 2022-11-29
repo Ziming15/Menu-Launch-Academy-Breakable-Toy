4a42bb7d666a499edd73894c0dbde2b2654f1d88
Ziming Chen <zimingchen153@gmail.com>
2022-11-28 21:06:26 -0500

    testing Food seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
 user_admin = User.create(
   email: "admin@gmail.com",
   password: "adminpassword",
   username: "admin",
   role: "admin"
+)
+Food.create(
+  name: "test123",
+  image_url:
+   "https://thumbs.dreamstime.com/b/lentil-tomato-soup-bowl-garmished-chives-more-url-http-www-istockphoto-com-file-search-php-action-lightboxid-here-116246592.jpg",
+  flavor: "Spicy",
+  description: "tst",
+  restaurant_id: "kP1b-7BO_VhWk_0tvuA_tw",
+  user_id: 3
 )
