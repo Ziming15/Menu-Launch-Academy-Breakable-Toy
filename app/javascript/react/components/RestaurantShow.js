1e36509c431b52958c6e4234202a909078bef941
Ziming Chen <zimingchen153@gmail.com>
2022-12-03 16:42:37 -0500

    edited categories for restaurant to be a sentence

--- app/javascript/react/components/RestaurantShow.js
+++ app/javascript/react/components/RestaurantShow.js
@@ -4,26 +4,28 @@ import ImageSlider from "./ImageSlider";
 const RestaurantShow = (props) => {
   let address;
   if (props.address) {
-    address = props.address.display_address.toString();
+    address = props.address.display_address.join(" ")
   }
 
   const categoryArray = props.categories.map((category) => {
-    return category.title
-  })
+    return category.title;
+  });
+
+  const categorySentence = new Intl.ListFormat().format(categoryArray)
   return (
     <>
       <h1 className="restaurant-name">{props.name}</h1>
       <div className="containerStyles">
         <ImageSlider slides={props.photosArray} />
       </div>
-        <div className="restaurant-text">
-          <p>Address: {address}</p>
-          <p>Rating: {props.rating}</p>
-          <p>Pricing: {props.price}</p>
-          <p>Categories: {categoryArray}</p>
-          <p>Phone Number: {props.phone}</p>
-          <p>Closed?: {props.closed}</p>
-        </div>
+      <div className="restaurant-text">
+        <p>Address: {address}</p>
+        <p>Rating: {props.rating}</p>
+        <p>Pricing: {props.price}</p>
+        <p>Categories: {categorySentence}</p>
+        <p>Phone Number: {props.phone}</p>
+        <p>Closed?: {props.closed}</p>
+      </div>
     </>
   );
 };
